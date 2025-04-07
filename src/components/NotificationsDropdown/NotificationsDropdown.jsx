4da2242b90155b879d2699a9e918e81056dc81d4
Barrett Anderson <barrett1614@gmail.com>
2025-04-07 10:23:45 -0500

    Working on Styling

--- src/components/NotificationsDropdown/NotificationsDropdown.jsx
+++ src/components/NotificationsDropdown/NotificationsDropdown.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-// import "./Notifications.css";
+import "./NotificationsDropdown.css";
 
 const NotificationsDropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
